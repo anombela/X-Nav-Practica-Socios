9fdd810315b7c70cdf10a666ae359243c1395645
Alfonso Nombela Moreno <anombela@zeta40.aulas.gsyc.es>
2016-04-25 14:57:41 +0200

    avanzando

--- socios.js
+++ socios.js
@@ -1,43 +1,45 @@
 $(document).ready(function() {
 
-   
+
     //$( "#accordion1").accordion();
-  
+
     $( "#tabs").tabs();
 
 
-
-    
     $.getJSON("json/myline.json").done(function(messages){
-        printmyline(messages);
+        printmessages(messages,0);
+    });
+    $.getJSON("json/timeline.json").done(function(messages){
+        printmessages(messages,1);
     });
 
-    
 
+    function printmessages(messages,x){
 
-
-
-
-
-    function printmyline(messages){
-
-        $("#accordion-myline").html("");
         var html = "";
         messages.forEach(function(message,i){
-        
-            html += "<h3>" + message.autor + "</h3>";
-            html += "<div>" + message.autor + "</div>";
-
+            html += "<h3>" +
+                    "<img src='" + message.avatar + "'>" +
+                    "<span class='autor'>" + message.autor + "</span>" +
+                    "<span class='titulo'>--" + message.titulo + "--</span>" +
+                    "</h3>";
+            html += "<div>" +
+                    ">>" + message.fecha + "<<" +
+                    "<p><span class='contenido'>" + message.contenido + "</span></p>" +
+                    "</div>";
         });
 
-        console.log(html)
-        $("#accordion-myline").append(html);
+        if (x==0){
+          $("#accordion-myline").append(html);
+          $("#accordion-myline").accordion({ active: true },{ collapsible: true }, { heightStyle: "content" });
+        }else{
+          $("#accordion-timeline").append(html);
+          $("#accordion-timeline").accordion({ active: true },{ collapsible: true }, { heightStyle: "content" });      
+        }
 
-            
-        $("#accordion1").accordion({ active: true },{ collapsible: true }, { heightStyle: "content" });
-        $("#accordion-myline").accordion({ active: true },{ collapsible: true }, { heightStyle: "content" });
-        
-        
-        
+
+
+
+
     };
 });
